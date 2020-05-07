9b97fc93201f898894ae14cb72d55033f83f5662
Max <justmaxfeeling@gmail.com>
2020-05-07 21:41:56 +0300

    Sixth lesson

--- src/main/java/com/example/SpringBoot2/controller/MainController.java
+++ src/main/java/com/example/SpringBoot2/controller/MainController.java
@@ -25,7 +25,7 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false; defaultValue = "") String  filter, Model model){
+    public String main(@RequestParam(required = false, defaultValue = "") String  filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
 
         if(filter != null && !filter.isEmpty()){
